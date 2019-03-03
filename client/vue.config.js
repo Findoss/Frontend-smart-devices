d98a85bb66da3bfe13836c31ebdffdf6cbb039f3
Nikita Stroganov <findoss@yandex.ru>
2019-03-04 01:27:48 +0300

    del maps-source

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  configureWebpack: (config) => {
+    if (process.env.NODE_ENV === 'production') {
+      config.devtool = '';
+    }
+  },
   pluginOptions: {
     i18n: {
       locale: 'en',
